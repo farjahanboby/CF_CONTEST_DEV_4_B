82bce13a5eee23afe5570b0e1296262e8ca64e53
Farjahan Akter Boby <93110981+farjahanboby@users.noreply.github.com>
2023-02-04 08:51:15 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,61 @@
 #include <iostream>
-
+#include<string>
 using namespace std;
 
 int main()
 {
-    int n,i,l;
-    string s;
+    int i,j,n,m;
     cin>>n;
+
     for(i=0;i<n;i++){
-        cin>>l;
-        for(i=0;i<l;i++){
-            cin>>s;
-                for(i=1;i<s.length();i++){
-                if(s[i]>s[i+1]){
-                       cout<<i<<endl;
+             int x=0,y=0,co=0;
+        cin>>m;
+        string s;
+        cin>>s;
+        for(j=0;j<m;j++){
+            if(s[j]=='U')
+            {
+         //       x=x;
+                y++;
+                if(x>=1&& y>=1 && x==y){
+                    co++;
                 }
-
             }
+            else if(s[j]=='D')
+                 {
+           //     x=x;
+                y--;
+               // if(x==y){
+               if(x>=1&& y>=1 && x==y){
+                    co++;
+                }
+            }
+            else if(s[j]=='R')
+                 {
+                x++;
+             //   y=y;
+               // if(x==y){
+               if(x>=1&& y>=1 && x==y){
+                    co++;
 
+                }
+            }
+            else if(s[j]=='L')
+                 {
+                x--;
+          //      y=y;
+                //if(x==y){
+                if(x>=1&& y>=1 && x==y){
+                    co++;
+
+                }
+            }
         }
+        if(co>0)
+    cout<<"YES"<<endl;
+   else
+    cout<<"NO"<<endl;
     }
-    return 0;
+
+return 0;
 }
